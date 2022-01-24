f06da012edb599d46f04b6e56a56640c3398b7ab
aliaksandrmikhailau <dqbblyvhyldghjy4bcapcx2gnyesoohor2mbvkkunclsussc46jq>
2022-01-25 00:13:42 +0300

    basic structure

--- golang-knowledge-base-frontend/src/app/app.module.ts
+++ golang-knowledge-base-frontend/src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { SidebarMenuComponent } from './sidebar-menu/sidebar-menu.component';
+import { ArticleListComponent } from './article-list/article-list.component';
+import { ArticleContentComponent } from './article-content/article-content.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarMenuComponent,
+    ArticleListComponent,
+    ArticleContentComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
